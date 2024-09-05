c23a02e943bf8f85e98b09d23f62bdaf0fe19e65
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-09-05 12:17:45 -0500

    9/5/2024

--- src/Q1/LP3_13.java
+++ src/Q1/LP3_13.java
@@ -3,12 +3,22 @@ import Utils.Console;
 
 public class LP3_13 {
     public static void main(String[] args) {
-        int p = Console.input("Enter the principal: ");
-        int y = Console.input("Enter the number of years: ");
-        double i = Console.input("Enter the interest rate: ");
+        String ans = Console.input("Enter p for Principal or a for Amount");
+        if (ans.equals("p")) {
+            int p = Console.input("Enter the principal: ");
+            int y = Console.input("Enter the number of years: ");
+            double i = Console.input("Enter the interest rate: ");
 
-        double v = p * (1 + y * i);
-        System.out.printf("The value after the term is: $%.2f", v);
+            double v = p * (1 + y * i);
+            System.out.printf("The value after the term is: $%.2f", v);
+        } else {
+            int a = Console.input("Enter the amount: ");
+            int y = Console.input("Enter the number of years: ");
+            double i = Console.input("Enter the interest rate: ");
+
+            double v = a / (1 + y * i);
+            System.out.printf("The principal after the term is: $%.2f", v);
+        }
     }
 }
 /*
