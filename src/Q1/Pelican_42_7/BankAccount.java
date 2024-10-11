68aad9d548296a2725d01722d6cfe898a82f1eb1
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-10-11 12:16:40 -0500

    10/11/2024

---
+++ src/Q1/Pelican_42_7/BankAccount.java
@@ -0,0 +1,11 @@
+package Q1.Pelican_42_7;
+
+public class BankAccount {
+    public String name;
+    public double balance;
+
+    public BankAccount(String nm, double amt) {
+        name = nm;
+        balance = amt;
+    }
+}
