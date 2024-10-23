960ec5dc22f3d32b852a4ff29e12f13be4304c5d
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-10-23 12:16:45 -0500

    10/23/2024

---
+++ src/Q1/Prog1060b.java
@@ -0,0 +1,14 @@
+package Q1;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Prog1060b {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Queue<String> q = new LinkedList<>();
+        while (true) {
+            System.out.println("");
+        }
+    }
+}
