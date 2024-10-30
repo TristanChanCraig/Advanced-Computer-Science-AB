575743464c04450aec8df5b5c7ad5cc37dd6feb7
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-10-30 12:18:23 -0500

    10/30/2024

--- src/Q1/Prog1071hStackFromQueue.java
+++ src/Q1/Prog1071hStackFromQueue.java
@@ -31,6 +31,10 @@ public class Prog1071hStackFromQueue {
             return value;
         }
 
+        public void setValue(int v) { value = v; }
+
+        public void sell(int amt) { quantity -= amt; }
+
         @Override
         public int compareTo(Cl1071h o) {
             return 0;
@@ -41,6 +45,7 @@ public class Prog1071hStackFromQueue {
             var file1 = new Scanner(new File("Langdat/bginvtry.txt"));
             var file2 = new Scanner(new File("Langdat/buysell.txt"));
             QueueStack<Cl1071h> qs = new QueueStack<>();
+            System.out.println("Merchandise Inventory:");
             while (file1.hasNext()) {
                 int code = file1.nextInt();
                 int quantity = file1.nextInt();
@@ -49,19 +54,35 @@ public class Prog1071hStackFromQueue {
                 System.out.printf("%d\t\t%d\t\t%.2f\n", code, quantity, value);
             }
             file1.close();
+            System.out.println();
 
+            System.out.println("Ending Merchandise Inventory:");
             while (file2.hasNext()) {
                 String c = file2.next();
+                var temp = qs;
                 if (c.equalsIgnoreCase("B")) {
                     int code = file2.nextInt();
                     int quantity = file2.nextInt();
                     double cost = file2.nextDouble();
+                    while (code != temp.peek().getCode()) temp.pop();
+                    Cl1071h jon = temp.pop();
+                    jon.sell(quantity);
+                    System.out.printf("%d\t\t%d\t\t%.2f\n", jon.getCode(), jon.getQuantity(), jon.getValue());
                 } else {
                     int code = file2.nextInt();
                     int quantity = file2.nextInt();
+                    while (code != temp.peek().getCode()) temp.pop();
+                    Cl1071h jon = temp.pop();
+                    jon.sell(quantity);
+                    System.out.printf("%d\t\t%d\t\t%.2f\n", jon.getCode(), jon.getQuantity(), jon.getValue());
                 }
             }
             file2.close();
+
+            for (Cl1071h die = qs.pop(); !qs.isEmpty();) {
+                System.out.printf("%d\t\t%d\t\t%.2f\n", die.getCode(), die.getQuantity(), die.getValue());
+                die = qs.pop();
+            }
 
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
