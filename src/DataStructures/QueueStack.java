8ced7ead1796879d22aba28fe4074e2569a17c72
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-10-31 12:16:38 -0500

    10/31/2024

--- src/DataStructures/QueueStack.java
+++ src/DataStructures/QueueStack.java
@@ -8,11 +8,10 @@ public class QueueStack<T extends Comparable<T>> {
     public void push(T element) { stack.enqueue(element); }
     public T pop()              {
         var temp = new Queue<T>();
-        while (int lcv = 0; lcv < stack.size() - 1; lcv++) temp.enqueue(stack.dequeue());
+        while () temp.enqueue(stack.dequeue());
         return temp;
     }
     public T peek()             { return stack.peek(); }
-    public int size()           { return stack.size(); }
     public boolean isEmpty()    { return stack.isEmpty(); }
 }
 // First-In Last-Out or Last-In First-Out
