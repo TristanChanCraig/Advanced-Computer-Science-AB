6f24def242584f53ddd9c87f5b22adc7c78dc70a
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-11-11 12:15:29 -0600

    11/11/2024

---
+++ src/Q2/CircularLLLab.java
@@ -0,0 +1,27 @@
+package Q2;
+import DataStructures.CircularLinkedList;
+
+public class CircularLLLab {
+    public static void main(String[] args) {
+        var list = new CircularLinkedList<Integer>();
+        System.out.println(list.size());
+        list.add(5);
+        list.add(7);
+        list.add(13);
+        System.out.println(list.size());
+
+        list.print();
+
+        list.remove(1);
+        list.print();
+
+        try {
+            list.remove(10000000);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("No such index");
+        }
+
+        for (var x : list) System.out.print(x + " ");
+        System.out.println();
+    }
+}
