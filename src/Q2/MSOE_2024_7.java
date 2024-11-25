87f532811024f14a67f80f9e876221883660518d
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-11-25 12:15:53 -0600

    11/25/2024

---
+++ src/Q2/MSOE_2024_7.java
@@ -0,0 +1,7 @@
+package Q2;
+
+public class MSOE_2024_7 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Q2/Prog285bBinaryTree.java
@@ -0,0 +1,53 @@
+package Q2;
+
+import DataStructures.LinkedListStack;
+import Q1.Prog285b.Cl285b;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import Q1.Prog285b.Cl285b;
+import DataStructures.BinarySearchTree;
+
+public class Prog285bBinaryTree {
+    public static void main(String[] args) {
+        try {
+            var file = new Scanner(new File("Langdat/prog285b.dat"));
+            var tree = new BinarySearchTree<Cl285b>();
+
+            while (file.hasNext()) {
+                int id = file.nextInt();
+                int c = file.nextInt();
+                double s = file.nextDouble();
+
+                var fred = new Cl285b(id, c, s);
+                tree.insert(fred);
+            }
+            file.close();
+
+            System.out.println("Number\tCode\tSales\t\tCommission");
+            tree.printInOrder();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+/*
+Number	Code	Sales		Commission
+101		17		$2250.00		$213.75
+103		5		$4000.00		$300.00
+117		3		$7350.00		$0.00
+118		8		$7350.00		$574.75
+125		5		$6500.00		$502.50
+138		17		$6375.00		$677.50
+192		8		$8125.00		$640.63
+203		8		$3250.00		$243.75
+218		5		$5000.00		$375.00
+235		5		$5250.00		$396.25
+264		17		$4150.00		$410.50
+291		17		$750.00		$71.25
+
+
+Process finished with exit code 0
+
+ */
