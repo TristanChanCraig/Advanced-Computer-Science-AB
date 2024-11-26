ca279e216943cbee61d88c2bfcea6c2a10f84da4
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-11-26 12:15:43 -0600

    11/26/2024

--- src/Q2/MSOE_2024_8.java
+++ src/Q2/MSOE_2024_8.java
@@ -5,6 +5,7 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import Algorithms.SortingAlgorithms;
 
 public class MSOE_2024_8 {
     public static void main(String[] args) {
@@ -13,23 +14,71 @@ public class MSOE_2024_8 {
         String[] numLetters = new String[100];
 
         String temp = input.nextLine();
-        while (temp != "") {
+        while (!temp.equals("")) {
             arrlist.add(temp);
             temp = input.nextLine();
         }
 
-        String key = arrlist.get(0);
+        String regex = arrlist.get(0).replaceAll("(?<=.)(?=.)", "+");
+        Pattern p = Pattern.compile(regex);
         for (int i = 1; i < arrlist.size(); i++) {
-            String word = arrlist.get(i);
-            for (int j = 0; j < key.length(); j++) {
-                boolean inside = word.find(key.substring(j, j+1));
-                if ()
+            Matcher m = p.matcher(arrlist.get(i));
+            boolean mFound = m.find();
+            if (mFound) {
+                arrlist.remove(i);
+                i--;
             }
         }
 
+        for (int i = 1; i < arrlist.size(); i++) {
+            String regex2 = arrlist.get(i).replaceAll("(?<=.)(?=.)", "+");
+            Pattern p2 = Pattern.compile(regex2);
+            Matcher m2 = p2.matcher(arrlist.get(0));
+            boolean mFound2 = m2.find();
+            if (mFound2) {
+                arrlist.remove(i);
+                i--;
+            }
+        }
+
+        //for (var w : arrlist) System.out.println(w);
+
+        String key = arrlist.get(0);
+        for (int i = 1; i < arrlist.size(); i++) {
+            String word = arrlist.get(i);
+            String t = word;
+            for (int j = 0; j < key.length(); j++) {
+                for (int k = j + 1; k < key.length() + 1; k++) {
+                    var w = key.substring(j, k);
+                    if (word.contains(w)) {
+                        int index = w.length() - 1;
+                        if (numLetters[index] == null) numLetters[index] = word;
+                        else numLetters[index] = numLetters[index] + " " + word;
+                    }
+                }
+            }
+        }
+
+
+
+        int highest = 0;
+        for (int i = numLetters.length-1; i >= 0; i--) {
+            if (numLetters[i] != null) {
+                highest = i;
+                break;
+            }
+        }
+
+        if (highest == 0) {
+            System.out.print("No embeddings found!");
+        } else {
+            String[] words = numLetters[highest].split(" ");
+            SortingAlgorithms.quickSort(words);
+            for (var word : words) System.out.println(word);
+        }
     }
 }
-/*
+/*-
 grand
 are
 bandersnatch
@@ -40,4 +89,8 @@ grandfather
 great
 hand
 plant
+
+bandersnatch
+branch
+hand
  */
