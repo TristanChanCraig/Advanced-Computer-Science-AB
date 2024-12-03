a8d1a8380d04fd6af32411a012d0ffc8a84fe07b
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2024-12-03 10:31:10 -0600

    12/2/2024

--- src/Q2/MSOE_2024_9.java
+++ src/Q2/MSOE_2024_9.java
@@ -1,43 +1,42 @@
 package Q2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MSOE_2024_9 {
-    public int recursion(String[][] map, int r, int c, int spiders) {
-        if (map[r][c].equalsIgnoreCase("f")) return spiders;
-        if (map[r][c].equalsIgnoreCase("s")) spiders++;
+    public static int recursion(String[][] map, int r, int c, int spiders) {
         try {
-            int d1 = recursion(map, r - 1, c - 1, spiders);
-            int d2 = recursion(map, r - 1, c - 1, spiders);
-            int d3 = recursion(map, r - 1, c - 1, spiders);
-            int d4 = recursion(map, r - 1, c - 1, spiders);
-            int d5 = recursion(map, r - 1, c - 1, spiders);
-            int d6 = recursion(map, r - 1, c - 1, spiders);
-            int d7 = recursion(map, r - 1, c - 1, spiders);
-            int d8 = recursion(map, r - 1, c - 1, spiders);
-            int d9 = recursion(map, r - 1, c - 1, spiders);
-        } catch (IndexOutOfBoundsException e) {
+            if (map[r][c].equalsIgnoreCase("f")) return spiders;
+            if (map[r][c].equalsIgnoreCase("s")) spiders++;
+            int d1 = recursion(map, r, c + 1, spiders);
+            int d2 = recursion(map, r + 1, c, spiders);
+            int d3 = recursion(map, r + 1, c + 1, spiders);
+            int d3 = recursion(map, r + 1, c + 1, spiders);
+            int[] arr = {d2, d3, d5, d7, d8};
+            Arrays.sort(arr);
+            return arr[0];
+        } catch (IndexOutOfBoundsException | NullPointerException e) {
             return 99999;
         }
     }
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String[][] map = new String[30][30];
-        String temp = input.nextLine();
-        int cnt = 0;
-        int eR;
-        int eC;
-        while (!temp.equals("")) {
-            for (int i = 0; i < temp.length(); i++) {
-                map[cnt][i] = temp.substring(i, i+1);
-                if (map[cnt][i].equalsIgnoreCase(">")) {
-                    eR = cnt;
-                    eC = i;
-                }
-            }
-            temp = input.nextLine();
-            cnt++;
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(new File("Langdat/9a.txt"));
+        ArrayList<String> arrlist = new ArrayList<>();
+        while (input.hasNext()) {
+            arrlist.add(input.nextLine());
         }
+        String[][] map = new String[arrlist.size()][arrlist.get(0).length()];
+        for (int i = 0; i < map.length; i++) for (int j = 0; j < map[0].length; j++) map[i][j] = arrlist.get(i).substring(j, j+1);
+        System.out.println("The least amount of spiders to get to the food is " + recursion(map, 0, 0, 0));
+        /*
+        for (String[] r : map) {
+            for (String c : r) System.out.print(c + " ");
+            System.out.println();
+        }
+        */
     }
 }
