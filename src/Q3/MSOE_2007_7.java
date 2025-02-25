4281a38b0823d1800090587d8e4aba081ff30f64
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2025-02-25 12:09:57 -0600

    2/25/2024

--- src/Q3/MSOE_2007_7.java
+++ src/Q3/MSOE_2007_7.java
@@ -9,11 +9,30 @@ public class MSOE_2007_7 {
         int digits = input.nextInt();
         System.out.print("Current reading: ");
         int reading = input.nextInt();
-        for (int i = 1; i < digits; i++) {
+        int total = 0;
+        int digits2 = digits;
+        for (int i = 0; i < digits2 / 2 + 1; i++) {
             int left = (int) Math.floor(reading / Math.pow(10, digits-1));
             if (left > reading % 10) {
-
+                total += (left - reading % 10) * Math.pow(10, i);
+            } else if (left < reading % 10) {
+                int multiplier = (int) Math.pow(10, i);
+                if (i == 0) {
+                    multiplier = 0;
+                    total += 10 + left - (reading % 10);
+                    reading += 10 + left - (reading % 10);
+                } else if (digits == 2) {
+                    total += (10 + left - (reading % 10)) * multiplier + multiplier;
+                    reading += (10 + left - (reading % 10)) * multiplier + multiplier;
+                } else {
+                    total += (10 + left - (reading % 10)) * multiplier;
+                    reading += (10 + left - (reading % 10));
+                }
+                reading %= Math.pow(10, digits-1);
+                reading /= 10;
+                digits -= 2;
             }
         }
+        System.out.println("Distance: " + total);
     }
 }
