b1fc63e9001452913a63dec57d56886a72ecc68f
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2025-03-05 11:52:01 -0600

    3/5/2025

--- src/Q3/Prog595r.java
+++ src/Q3/Prog595r.java
@@ -13,7 +13,8 @@ public class Prog595r {
             var file = new Scanner(new File("Langdat/dale-chall-word-list.txt"));
             var input = new Scanner(System.in);
 
-            System.out("Enter the Sentence: ");
+            System.out.print("Enter a sentence to calculate the grade level: ");
+            var line = input.nextLine();
 
             var words = new ArrayList<String>();
             while (file.hasNext()) {
@@ -21,24 +22,24 @@ public class Prog595r {
             }
             file.close();
 
-            var newSen = new ArrayList<String>();
-            for (String s : words) {
-                var temp = s.split("[.?!]");
-                newSen.addAll(Arrays.asList(temp));
-            }
+            var temp = line.split("[.?!]");
+            var newSen = new ArrayList<String>(Arrays.asList(temp));
             int numSen = newSen.size();
+
             var newWords = new ArrayList<String>();
-            for (String word : words) {
-                var temp = word.split("\\s+");
-                newWords.addAll(Arrays.asList(temp));
-            }
+            var temp2 = line.split("\\s+");
+            newWords.addAll(Arrays.asList(temp2));
             newWords.replaceAll(n -> n.replaceAll("[^a-zA-Z]", "").toLowerCase());
             int numWords = newWords.size();
-            int diffWords = 3000 - numWords;
+
+            var list = newWords.stream().filter(n -> !words.contains(n)).toList();
+            int diffWords = list.size();
+
             double unfamiliarWords = (double)diffWords/numWords;
             double avgSenLength = numWords / (double) numSen;
             double rawScore = 0.1579*(unfamiliarWords*100) + 0.0496*(avgSenLength);
             if (unfamiliarWords > 0.05) rawScore += 3.6365;
+
             System.out.print("Raw Score: " + rawScore + "\t\t");
             if (rawScore < 5) System.out.print("Grades K-4");
             else if (rawScore < 6) System.out.print("Grades 5-6");
@@ -53,3 +54,19 @@ public class Prog595r {
         }
     }
 }
+/*
+Enter a sentence to calculate the grade level: The quick brown fox jumps over the lazy dog. Many installations require thorough understanding.
+Raw Score: 9.622985714285715		Grades 13-15 (College)
+
+Enter a sentence to calculate the grade level: Jonah is a bully who will never be nice in his lifetime. He will always scrutinize the familiarity of warming passions.
+Raw Score: 9.420633333333333		Grades 13-15 (College)
+
+Enter a sentence to calculate the grade level: history is nice
+Raw Score: 0.1488		Grades K-4
+
+Enter a sentence to calculate the grade level: cats and dog are humiliating the public
+Raw Score: 8.495128571428571		Grades 11-12
+
+Enter a sentence to calculate the grade level: skibidi toilet rizz
+Raw Score: 14.311966666666665		Grades 16 and above (College Graduate)
+ */
