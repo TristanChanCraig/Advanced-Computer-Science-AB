60f46455d7eb7fab74f0c1e2fc9e3b804f0f8bff
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2025-04-04 12:17:11 -0500

    4/2/2025

--- src/Q3/MSOE_2009_8.java
+++ src/Q3/MSOE_2009_8.java
@@ -2,9 +2,32 @@ package Q3;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class MSOE_2009_8 {
+    public static void print(String[][] mat) {
+        for (String[] arr : mat) {
+            for (String str : arr) System.out.print(str + " ");
+            System.out.println();
+        }
+    }
+
+    public static void nextTo(String[][] mat, int prevI, int prevJ, int i, int j) {
+        if ((i < 0 || i > mat.length - 1) || (j < 0 || j > mat[0].length - 1)) return;
+        if (Objects.equals(mat[i][j], "*") && !Objects.equals(mat[prevI][prevJ], "*")) {
+            mat[i][j] = mat[prevI][prevJ];
+            nextTo(mat, i, j, i - 1, j - 1);
+            nextTo(mat, i, j, i - 1, j);
+            nextTo(mat, i, j, i - 1, j + 1);
+            nextTo(mat, i, j, i, j - 1);
+            nextTo(mat, i, j, i, j + 1);
+            nextTo(mat, i, j, i + 1, j - 1);
+            nextTo(mat, i, j, i + 1, j);
+            nextTo(mat, i, j, i + 1, j + 1);
+        }
+    }
+
     public static void main(String[] args) {
         try {
             var file = new Scanner(new File("Langdat/image1.txt"));
@@ -16,32 +39,46 @@ public class MSOE_2009_8 {
             file.nextLine();
             for (int i = 0; i < row; i++) {
                 String line = file.nextLine();
-                for (int j = 0; j < col; j++) {
+                for (int j = 0; j < line.length(); j++) {
                     first[i][j] = line.substring(j, j+1);
                 }
             }
             file.close();
-            int row2 = file.nextInt();
-            int col2 = file.nextInt();
+            int row2 = file2.nextInt();
+            int col2 = file2.nextInt();
             String[][] second = new String[row2][col2];
             file2.nextLine();
             for (int i = 0; i < row2; i++) {
                 String line = file2.nextLine();
-                for (int j = 0; j < col2; j++) {
+                for (int j = 0; j < line.length(); j++) {
                     second[i][j] = line.substring(j, j+1);
                 }
             }
             file2.close();
 
-            for (int i = 0; i < row; i++) {
-                for (int j = 0; j < col; j++) {
-                    if (i == 0) {
-                        if (j == 0) {
+            for (int i = 0; i < first.length; i++) for (int j = 0; j < first[0].length; j++) if (first[i][j] == null) first[i][j] = "";
+            for (int i = 0; i < second.length; i++) for (int j = 0; j < second[0].length; j++) if (second[i][j] == null) second[i][j] = "";
 
-                        }
+            var alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int index = 0;
+            for (int i = 0; i < first.length - 1; i++) {
+                for (int j = 0; j < first[0].length - 1; j++) {
+                    if (Objects.equals(first[i][j], "*")) {
+                        first[i][j] = alph.substring(index, index+1);
+                        index++;
+                        nextTo(first, i, j, i - 1, j - 1);
+                        nextTo(first, i, j, i - 1, j);
+                        nextTo(first, i, j, i - 1, j + 1);
+                        nextTo(first, i, j, i, j - 1);
+                        nextTo(first, i, j, i, j + 1);
+                        nextTo(first, i, j, i + 1, j - 1);
+                        nextTo(first, i, j, i + 1, j);
+                        nextTo(first, i, j, i + 1, j + 1);
                     }
                 }
             }
+            print(first);
+
 
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
