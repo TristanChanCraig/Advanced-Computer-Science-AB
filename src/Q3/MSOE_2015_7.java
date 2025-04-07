0a365431e817b5c5550cd13a068ffe24a943760d
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2025-04-07 09:55:25 -0500

    4/7/2025

--- src/Q3/MSOE_2015_7.java
+++ src/Q3/MSOE_2015_7.java
@@ -4,25 +4,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MSOE_2015_7 {
-    public static int[] helper(int start, int[] arr, int target) {
-        if (start >= 3) return arr;
-        int cnt = 0;
-        for (int i = start; i < 3; i++) cnt += arr[i];
-        if (cnt == target) return arr;
-        for (int i = target; i >= 0; i--) {
-            double t = i*(i+1)/(double)2;
-            if (t % 1 == 0 && t < target) {
-                int[] temp = Arrays.copyOf(arr, 3);
-                temp[start] = (int) t;
-                int[] e = helper(start+1, temp, (int) (target-t));
-                int c = 0;
-                for (int j = 0; j < 3; j++) c += arr[j];
-                if (c == target) return arr;
-            }
-        }
-        return new int[3];
-    }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number to be partitioned: ");
@@ -30,7 +11,32 @@ public class MSOE_2015_7 {
         input.close();
 
         System.out.print("The array is: ");
-        int[] arr = helper(0, new int[3], num);
+        int[] arr = new int[3];
+        for (int i = 0; i <= num; i++) {
+            int t1 = (i*(i+1))/2;
+            for (int j = 0; j <= num; j++) {
+                int t2 = (j*(j+1))/2;
+                for (int k = 0; k <= num; k++) {
+                    int t3 = (k*(k+1))/2;
+                    if (t1 + t2 + t3 == num) {
+                        arr = new int[]{t1, t2, t3};
+                    }
+                }
+            }
+        }
         for (int n : arr) System.out.print(n + " ");
     }
 }
+/*
+Enter the number to be partitioned: 20
+The array is: 10 10 0
+
+Enter the number to be partitioned: 1
+The array is: 1 0 0
+
+Enter the number to be partitioned: 6
+The array is: 6 0 0
+
+Enter the number to be partitioned: 30
+The array is: 28 1 1
+ */
