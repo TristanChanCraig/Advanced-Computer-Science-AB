429bf3d4c0514cb660cf7b7b0252fcbe0d5ff853
chan.t3 <chan.t3@stu.janesville.k12.wi.us>
2025-04-10 12:15:09 -0500

    4/10/2025

--- src/Q3/MSOE_2009_9.java
+++ src/Q3/MSOE_2009_9.java
@@ -2,18 +2,56 @@ package Q3;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MSOE_2009_9 { // use doubly linked lists?
+public class MSOE_2009_9 {
+    static class Person {
+        private int id;
+        private int[] connections;
+        private boolean[] did;
+
+
+        Person(int id, int con) {
+            this.id = id;
+            connections = new int[con];
+            did = new boolean[con];
+        }
+    }
+
     public static void main(String[] args) {
         try {
             var file = new Scanner(new File("Langdat/MSOE_2009_9.txt"));
 
             int n = file.nextInt();
+            int[][] mat = new int[n][n];
+            file.nextLine();
             while (file.hasNext()) {
-
+                String[] temp = file.nextLine().split(" ");
+                mat[Integer.parseInt(temp[0])][Integer.parseInt(temp[1])] = 1;
+                mat[Integer.parseInt(temp[1])][Integer.parseInt(temp[0])] = 1;
             }
             file.close();
+            for (int i = 0; i < mat.length; i++)
+                for (int j = 0; j < mat[0].length; j++)
+                    if (mat[i][j] == 0) mat[i][j] = 9999;
+
+            for (int i = 0; i < mat.length; i++) {
+                for (int j = 0; j < mat[0].length; j++) {
+                    int deg = mat[i][j];
+                    if (deg != 0 && deg != 9999) {
+                        for (int k = 0; k < mat[0].length; k++) {
+                            int currDeg = mat[i][k];
+                            if (currDeg + mat[j][k] < deg) deg = currDeg + mat[j][k];
+                        }
+                    }
+                }
+            }
+
+            for (int[] i : mat) {
+                for (int j : i) System.out.print(j + " ");
+                System.out.println();
+            }
 
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
